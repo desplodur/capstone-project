2729da347b5a525461385767495b2ccc39863356
Laurenz Rudolph <Laurenz.Rudolph@t-online.de>
2022-06-21 15:01:13 +0200

    clea code adjustments

--- src/components/QuestionComponent/styled.js
+++ src/components/QuestionComponent/styled.js
@@ -3,15 +3,16 @@ import styled from 'styled-components';
 import {pxToRem} from '../../utils/unit';
 
 const StyledQuestionComponent = styled.section`
+	width: 25rem;
+	height: 5rem;
+	margin-bottom: ${pxToRem(8)};
 	padding: 0.5em 1em;
 	border: 1px solid rgba(0, 0, 0, 0.3);
 	border-radius: ${pxToRem(8)};
 	background: #517664;
 	color: #fff;
-	width: 25rem;
-	height: 5rem;
+
 	font-size: 1em;
-	margin-bottom: ${pxToRem(8)}; ;
 `;
 
 export default StyledQuestionComponent;
