d3ef4ef2610374f627ef68f3efe157987f6807cc
Laurenz Rudolph <Laurenz.Rudolph@t-online.de>
2022-06-23 15:02:37 +0200

    create page, move questions

--- src/pages/questions.js
+++ src/pages/questions.js
@@ -1,26 +1,15 @@
 import {nanoid} from 'nanoid';
-import {useState} from 'react';
 import {Helmet} from 'react-helmet';
 
 import Layout from '../components/Layout';
 import QuestionComponent from '../components/QuestionComponent';
-import QuestionForm from '../components/QuestionForm';
 
-export default function HomePage() {
-	const [questions, setQuestions] = useState([
+export default function QuestionPage() {
+	const questions = [
 		{id: nanoid(), questionText: 'What is export default?'},
 		{id: nanoid(), questionText: 'What does clean code means?'},
 		{id: nanoid(), questionText: 'Why do we have to define keys?'},
-	]);
-
-	const createNewQuestion = event => {
-		event.preventDefault();
-		const newQuestion = {
-			id: nanoid(),
-			questionText: event.target.questionTextInput.value,
-		};
-		setQuestions([...questions, newQuestion]);
-	};
+	];
 
 	return (
 		<Layout>
@@ -28,8 +17,13 @@ export default function HomePage() {
 				<title key="title">My Project</title>
 				<meta key="description" name="description" content="This is my project" />
 			</Helmet>
-
-			<QuestionForm onSubmit={createNewQuestion} />
+			<article>
+				{questions.map(question => {
+					return (
+						<QuestionComponent key={nanoid}>{question.questionText}</QuestionComponent>
+					);
+				})}
+			</article>
 		</Layout>
 	);
 }
