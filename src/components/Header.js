a571af9586a87abf7fcc77c18e80c5c515a3dc8d
Laurenz Rudolph <Laurenz.Rudolph@t-online.de>
2022-06-23 11:22:12 +0200

    initial commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,3 @@
-import {Link} from 'react-router-dom';
-
 export default function Header() {
-	return (
-		<header>
-			<nav>
-				<Link to="/">Home</Link>
-				<Link to="/about">About me</Link>
-			</nav>
-		</header>
-	);
+	return <header></header>;
 }
--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -34,3 +34,4 @@ export default function useFetch(url) {
 
 	return state;
 }
+/***test */
