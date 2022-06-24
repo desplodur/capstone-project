868cdf0ca1e2c8693e759046b6f0394894183f09
Laurenz Rudolph <Laurenz.Rudolph@t-online.de>
2022-06-24 16:11:54 +0200

    feature done

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,3 @@
 export default function Header() {
-<<<<<<< HEAD
 	return <header></header>;
-=======
-	return (
-		<header>
-			<nav>
-				<Link to="/">Home</Link>
-				<Link to="/questions">Questions</Link>
-				<Link to="/profile">Profile</Link>
-			</nav>
-		</header>
-	);
->>>>>>> main
 }
