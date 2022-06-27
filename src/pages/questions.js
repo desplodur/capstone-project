2fbe7967fd94deb3548edea2f4de3e84d384bd60
Laurenz Rudolph <Laurenz.Rudolph@t-online.de>
2022-06-27 11:51:08 +0200

    feature done

--- src/pages/questions.js
+++ src/pages/questions.js
@@ -1,15 +1,46 @@
 import {nanoid} from 'nanoid';
+import {useState} from 'react';
 import {Helmet} from 'react-helmet';
 
 import Layout from '../components/Layout';
 import QuestionComponent from '../components/QuestionComponent';
 
 export default function QuestionPage() {
-	const questions = [
-		{id: nanoid(), questionText: 'What is export default?'},
-		{id: nanoid(), questionText: 'What does clean code means?'},
-		{id: nanoid(), questionText: 'Why do we have to define keys?'},
-	];
+	const [questions, setQuestions] = useState([
+		{
+			id: nanoid(),
+			questionText: 'What is export default?',
+			answers: [
+				{id: nanoid(), answerText: 'its ...'},
+				{id: nanoid(), answerText: 'it means that ...'},
+			],
+		},
+		{
+			id: nanoid(),
+			questionText: 'What does clean code means?',
+			answers: [{id: nanoid(), answerText: 'its ...'}],
+		},
+		{
+			id: nanoid(),
+			questionText: 'Why do we have to define keys?',
+			answers: [],
+		},
+	]);
+
+	const addAnswer = (event, id) => {
+		event.preventDefault();
+		const index = questions.findIndex(question => {
+			return id === question.id;
+		});
+		const newAnswer = {
+			id: nanoid(),
+			answerText: event.target.inputField.value,
+		};
+		const newQuestions = [...questions];
+		newQuestions[index].answers = [...newQuestions[index].answers, newAnswer];
+		setQuestions(newQuestions);
+		//setQuestions(newQuestions);
+	};
 
 	return (
 		<Layout>
@@ -24,7 +55,7 @@ export default function QuestionPage() {
 			<article>
 				{questions.map(question => {
 					return (
-						<QuestionComponent key={nanoid}>{question.questionText}</QuestionComponent>
+						<QuestionComponent key={nanoid} question={question} addAnswer={addAnswer} />
 					);
 				})}
 			</article>
