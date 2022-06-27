7cfadeda86bfc492c2693347cad6e1648de393fc
Laurenz Rudolph <Laurenz.Rudolph@t-online.de>
2022-06-27 12:07:56 +0200

    stylelint bug fix

--- src/components/QuestionComponent/styled.js
+++ src/components/QuestionComponent/styled.js
@@ -4,7 +4,6 @@ import {pxToRem} from '../../utils/unit';
 
 const StyledQuestionComponent = styled.section`
 	width: 20rem;
-
 	border: 1px solid rgba(0, 0, 0, 0.3);
 	border-radius: ${pxToRem(8)};
 `;
