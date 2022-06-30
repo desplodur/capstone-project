f40514f4a86d5a1785d4ae82fbf23355d6cf549c
Laurenz Rudolph <Laurenz.Rudolph@t-online.de>
2022-06-30 17:08:36 +0200

    add persist to zustand

--- src/hooks/useStore.js
+++ src/hooks/useStore.js
@@ -1,32 +1,41 @@
 import create from 'zustand';
+import {persist} from 'zustand/middleware';
 
 import questionDatabase from '../utils/questionDatabase';
 import userDatabase from '../utils/userDatabase';
 
-export const useStore = create(set => ({
-	questions: [...questionDatabase],
-	addNewAnswer: newQuestion => {
-		set(() => {
-			return {
-				questions: newQuestion,
-			};
-		});
-	},
-	addNewQuestion: newQuestion => {
-		set(state => {
-			return {
-				questions: [newQuestion, ...state.questions],
-			};
-		});
-	},
-	users: userDatabase,
-	activeUser: userDatabase[0],
-	setActiveUser: newActiveUser => {
-		console.log(newActiveUser);
-		set(() => {
-			return {
-				activeUser: newActiveUser,
-			};
-		});
-	},
-}));
+export const useStore = create(
+	persist(
+		set => ({
+			questions: [...questionDatabase],
+			addNewAnswer: newQuestion => {
+				set(() => {
+					return {
+						questions: newQuestion,
+					};
+				});
+			},
+			addNewQuestion: newQuestion => {
+				set(state => {
+					return {
+						questions: [newQuestion, ...state.questions],
+					};
+				});
+			},
+			users: userDatabase,
+			activeUser: userDatabase[0],
+			setActiveUser: newActiveUser => {
+				console.log(newActiveUser);
+				set(() => {
+					return {
+						activeUser: newActiveUser,
+					};
+				});
+			},
+		}),
+		{
+			name: 'food-storage', // unique name
+			getStorage: () => sessionStorage, // (optional) by default, 'localStorage' is used
+		}
+	)
+);
