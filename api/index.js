391cfb21f50aba09a606f6d2a18311b869d5abac
Laurenz Rudolph <Laurenz.Rudolph@t-online.de>
2022-07-11 17:31:12 +0200

    add post method

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import connectToMongodb from '../backend/db/connect-to-mongodb';
 import Question from '../backend/models/Question';
 import Answer from '../backend/models/Answer';
 import User from '../backend/models/User';
+import {nanoid} from 'nanoid';
 
 export default async function handler(request, response) {
 	await connectToMongodb();
@@ -12,6 +13,17 @@ export default async function handler(request, response) {
 		const users = await User.find({});
 		console.log(users);
 		return response.status(200).json({questions, answers, users});
+	}
+	if (request.method === 'POST') {
+		const newQuestion = new Question({
+			id: nanoid,
+			questionText: String,
+			answers: [String],
+			userID: String,
+			answered: Boolean,
+		});
+		await Question.save();
+		return response.status(201).json({data: newQuestion});
 	}
 	return response.status(403).json({message: 'Error: request method not allowed.'});
 }
