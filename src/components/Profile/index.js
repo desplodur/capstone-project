adf52c0ddfbcdab6f32f774cd244510ec203d21c
Laurenz Rudolph <Laurenz.Rudolph@t-online.de>
2022-07-16 16:57:27 +0200

    userAuth feature done

--- src/components/Profile/index.js
+++ src/components/Profile/index.js
@@ -3,7 +3,9 @@ import {useSession} from '@pforte/react';
 
 import Button from '../Button';
 
-import StyledProfile from './styled';
+import {StyledProfile} from './styled';
+import {StyledProfilePicture} from './styled';
+import {LogoutButton} from './styled';
 
 export default function ProfileComponent() {
 	const session = useSession();
@@ -11,30 +13,16 @@ export default function ProfileComponent() {
 	console.log(session);
 	return (
 		<StyledProfile>
-			<article>
-				{session && <img src={session.user.image} alt={session.user.name} />}
-				<h1>
-					Username
-					{session?.user.name ?? 'Loading...'}
-				</h1>
-				<label>Choose a Title:</label>
-				<select name="title">
-					<option value="beginner" selected>
-						Duckling
-					</option>
-					<option value="intermediate">Nerd</option>
-					<option value="advanced">Hacker</option>
-				</select>
-				<Button
-					greyButton={true}
-					type="button"
-					onClick={() => {
-						void signOut('github');
-					}}
-				>
-					Sign out
-				</Button>
-			</article>
+			{session && <StyledProfilePicture src={session.user.image} alt={session.user.name} />}
+			<h1>{'Username: ' + session?.user.name ?? 'Loading...'}</h1>
+			<LogoutButton
+				type="button"
+				onClick={() => {
+					void signOut('github');
+				}}
+			>
+				Sign out
+			</LogoutButton>
 		</StyledProfile>
 	);
 }
--- src/components/Profile/styled.js
+++ src/components/Profile/styled.js
@@ -1,13 +1,26 @@
 import styled from 'styled-components';
 
-const StyledProfile = styled.section`
+export const StyledProfile = styled.section`
 	margin: 0 auto;
 
-	article {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-	}
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+`;
+export const StyledProfilePicture = styled.img`
+	border-radius: 100%;
+	border: 0.1em solid var(--dark);
+	width: 60%;
+	margin: 2em;
 `;
 
-export default StyledProfile;
+export const LogoutButton = styled.button`
+	margin: 2em;
+	padding: 0.5em;
+	background-color: var(--dark);
+	width: 8em;
+	font-size: 2rem;
+	border: none;
+	border-radius: 8px;
+	color: var(--light);
+`;
