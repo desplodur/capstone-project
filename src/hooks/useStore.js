9a42ce4b018947fbc89f257651abd347133afb88
Laurenz Rudolph <96203909+desplodur@users.noreply.github.com>
2022-07-17 22:36:31 +0200

    Update useStore.js

--- src/hooks/useStore.js
+++ src/hooks/useStore.js
@@ -1,5 +1,5 @@
 import create from 'zustand';
 
 export const useStore = create(set => ({
-	activeUser: {_id: '62cd8668823139d893144b3c', userName: 'Michael'},
+	activeUser: {_id: '62cd8668823139d893144b3c', userName: 'Laurenz'},
 }));
